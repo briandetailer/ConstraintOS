8cf31f19a8378c5df299c81b9db6d47bd075b142
briandetailer <65026395+briandetailer@users.noreply.github.com>
2026-07-05 14:42:31 -0400

    Export runtime artifact APIs

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,5 +1,6 @@
 """ConstraintOS Runtime package."""
 
+from runtime.artifacts import ArtifactStore, ArtifactStoreError, RuntimeArtifact
 from runtime.context import RuntimeContext
 from runtime.engine import RuntimeEngine
 from runtime.events import RuntimeEvent, RuntimeEventType
@@ -7,6 +8,9 @@ from runtime.result import RuntimeResult
 from runtime.state import RuntimeState
 
 __all__ = [
+    "ArtifactStore",
+    "ArtifactStoreError",
+    "RuntimeArtifact",
     "RuntimeContext",
     "RuntimeEngine",
     "RuntimeEvent",
