e6dbb526ad348f40b0d59466cbcaae45ff5d96d6
briandetailer <65026395+briandetailer@users.noreply.github.com>
2026-07-05 11:10:12 -0400

    Export runtime orchestration primitives

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,1 +1,16 @@
 """ConstraintOS Runtime package."""
+
+from runtime.context import RuntimeContext
+from runtime.engine import RuntimeEngine
+from runtime.events import RuntimeEvent, RuntimeEventType
+from runtime.result import RuntimeResult
+from runtime.state import RuntimeState
+
+__all__ = [
+    "RuntimeContext",
+    "RuntimeEngine",
+    "RuntimeEvent",
+    "RuntimeEventType",
+    "RuntimeResult",
+    "RuntimeState",
+]
