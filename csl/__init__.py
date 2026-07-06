3a2098ec5cc531cea4b06de1d3c7fe36031eabee
briandetailer <65026395+briandetailer@users.noreply.github.com>
2026-07-06 19:32:30 -0400

    Export CSL validation result verifier

--- csl/__init__.py
+++ csl/__init__.py
@@ -1,9 +1,15 @@
 """Constraint Specification Language package."""
 
-from csl.contracts import CSL_CONTRACT_VERSION, CSLContractVerification, verify_csl_document_contract
+from csl.contracts import (
+    CSL_CONTRACT_VERSION,
+    CSLContractVerification,
+    verify_csl_document_contract,
+    verify_csl_validation_result_contract,
+)
 
 __all__ = [
     "CSL_CONTRACT_VERSION",
     "CSLContractVerification",
     "verify_csl_document_contract",
+    "verify_csl_validation_result_contract",
 ]
